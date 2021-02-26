3b51db99de55dc127f3e263dd293c2c5a2c964c9
Andrew Goacher <goach@hey.com>
2021-02-26 23:21:17 +0000

    Remove warnings on color constants

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,6 @@
+#[allow(dead_code)]
 pub const CORNFLOWER_BLUE: (f32, f32, f32, f32) = (100f32/255f32, 149f32/255f32, 237f32/255f32, 1f32);
+#[allow(dead_code)]
 pub const BLACK: (f32, f32, f32, f32) = (0f32, 0f32, 0f32, 1f32);
+#[allow(dead_code)]
 pub const WHITE: (f32, f32, f32, f32) = (1f32, 1f32, 1f32, 1f32);
